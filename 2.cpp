31e690ebc7de15ece333de2176ab9749f6cb559b
himanshu1577 <himanshukumardn@gmail.com>
2024-09-11 20:20:29 +0530

    file

--- 2.cpp
+++ 2.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main(){
-    
+    int n ;
+    cin>>n;
+    vector<int> inside(n);
+    vector<int> outside(n);
+    for(int i =0 ;i<n;i++){
+        cin>>inside[i];
+    }
+    for(int i = 0;i<n;i++){
+        cin>>outside[i];
+    }
+    int ans = INT_MIN;
+    for(int i =0 ;i<n;i++){
+        if(i == 0){
+            ans = max(ans,inside[i] - outside[i]);
+        }
+        else{
+            int t = ans + inside[i];
+            ans = max(ans,inside[i] - outside[i]);
+        }
+    }
+    cout<<ans<<endl;
 }
